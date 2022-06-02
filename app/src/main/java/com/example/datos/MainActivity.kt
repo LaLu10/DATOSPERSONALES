9e9727b6890e96c2227ffdb61f35b35c338d6d31
Alumno <Farahnahid137@gmail.com>
2022-06-02 20:25:05 +0200

    DATOS A MEDIAS

--- app/src/main/java/com/example/datos/MainActivity.kt
+++ app/src/main/java/com/example/datos/MainActivity.kt
@@ -15,40 +15,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnSend.setOnClickListener { getAndSend() }
-        var mapa = mutableListOf<String>()
-    }
-    fun getAndSend(){
-        val intentResult = Intent(this,ResultActivity ::class.java)
-        intentResult.putExtra("NOMBRE", binding.etInfo.text.toString())
-        intentResult.putExtra("AÑO", binding.etInfo.text.toString())
-        intentResult.putExtra("GENERO", binding.etInfo.text.toString())
-        intentResult.putExtra("EMAIL", binding.etInfo.text.toString())
-        intentResult.putExtra("TELEFONO", binding.etInfo.text.toString())
-        startActivity(intentResult)
-        var userSelect = ""
-        val adaptador: ArrayAdapter<*> = ArrayAdapter.createFromResource(this, R.array.DATA,
-            android.R.layout.simple_spinner_item)
+        var spinner = ""
+        val adaptador: ArrayAdapter<*> = ArrayAdapter.createFromResource(
+            this, R.array.DATA,
+            android.R.layout.simple_spinner_item
+        )
         binding.spnData.adapter = adaptador
-        binding.spnData.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        binding.spnData.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                userSelect = parent?.getItemAtPosition(position).toString()
-                Toast.makeText(this@MainActivity, userSelect, Toast.LENGTH_SHORT).show()
+                spinner = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, spinner, Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                userSelect = "No ha habido selección"
-                Toast.makeText(this@MainActivity, userSelect, Toast.LENGTH_SHORT).show()
+                spinner = "No ha habido selección"
+                Toast.makeText(this@MainActivity, spinner, Toast.LENGTH_SHORT).show()
             }
         }
-
+        var mapa = mutableMapOf<String, String>()
+        mapa.put(spinner, binding.etInfo.text.toString())
+        binding.btnSend.setOnClickListener {
+            val intentResult = Intent(this, ResultActivity::class.java)
+            intentResult.putExtra("NOMBRE", binding.etInfo.text.toString())
+            intentResult.putExtra("AÑO", binding.etInfo.text.toString())
+            intentResult.putExtra("GENERO", binding.etInfo.text.toString())
+            intentResult.putExtra("EMAIL", binding.etInfo.text.toString())
+            intentResult.putExtra("TELEFONO", binding.etInfo.text.toString())
+            startActivity(intentResult)
+        }
     }
-
 }
 
 
